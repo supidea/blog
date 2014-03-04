81fe3e32206d52ba2fa8d7bdf25372b7646e99a8
komla <komla@163.com>
2014-03-04 18:06:51 +0800

    20140304

--- models/Curd.js
+++ models/Curd.js
@@ -2,15 +2,21 @@ var Article=require('./Article');
 var Post=Article.Post;
 
 //获取一篇文章
-exports.readOnePost=function(req,res,queryStr){
+exports.readOnePost=function(req,res,queryStr,pageStr){
     Post.findOne(queryStr,function(err,doc){
-        res.render('article',{
+        res.render(pageStr,{
             post_title:doc.post_title
         })
     })
 }
 
-
+exports.readAllPost=function(req,res,queryStr,pageStr){
+    Post.find(queryStr,function(err,doc){
+        res.render(pageStr,{
+            doc:doc
+        })
+    })
+}
 
 
 //新建文章
--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ var Curd=require('../models/Curd')
 module.exports=function(app){
 
     app.get('/',function(req,res){
-        Curd.readOnePost(req,res,{})
+        Curd.readAllPost(req,res,{},'index')
     });
 
     app.get('/install',function(req,res){
         Curd.insertPost(req,res,{
-            post_title:'xxxx',
+            post_title:'xxxx2345',
             post_content:"a啊是打dddddddddd发打发打发dd"
         })
         res.send('数据库安装成功');
